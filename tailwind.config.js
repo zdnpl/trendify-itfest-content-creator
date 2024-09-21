f28d98ab791902999247bb36ad30b7779b3541f1
Zidan Naufal <83897266+zdnpl@users.noreply.github.com>
2024-09-21 18:00:40 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
+    "./src/**/*.{html,js,svelte,ts}",
     "./index.html", // Mengawasi file index.html
-    "./creator.html", // Mengawasi file creator.html di dalam folder views
-  ],
+    "./creator.html", // Mengawasi file creator.html di dalam folder views
+  ],
+
   theme: {
     extend: {
       colors: {
