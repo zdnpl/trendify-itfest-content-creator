f3c301dafa786a6654c6a8eb1b9a2d8cf9830a63
Zidan Naufal <83897266+zdnpl@users.noreply.github.com>
2024-09-21 16:44:46 +0700

    Delete src directory

--- src/main.js
+++
@@ -1,7 +0,0 @@
-setTimeout(() => {
-  document.getElementById("loader").style.display = "none";
-}, 1500);
-
-function closeLogin() {
-  document.getElementById("overlay").style.display = "none";
-}
